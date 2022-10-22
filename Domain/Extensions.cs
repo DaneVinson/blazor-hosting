88a7803541949119feea10b52a21dc68be8f92d6
DaneVinson <dane.vinson@gmail.com>
2022-10-21 21:32:18 -0700

    Small refactor

--- Domain/Extensions.cs
+++ Domain/Extensions.cs
@@ -7,6 +7,6 @@ namespace Domain
     public static class Extensions
     {
         public static string ToPrettyString(this decimal number) =>
-            string.Format("{0:#,##0.##}", number);
+            $"{number:#,##0.##}";
     }
 }
